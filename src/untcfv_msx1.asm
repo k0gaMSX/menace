ad147ecaf3e7a495878266fb0c23b02336cd05d5
bifi <bifi@b7dc2feb-abe1-49e5-b0b8-892bdb346a61>
2009-12-12 15:28:42 +0000

    removed VDP port assumptions and fixed a bug (GuyveR800)
    git-svn-id: svn+ssh://localhost/var/local/svn/menace@110 b7dc2feb-abe1-49e5-b0b8-892bdb346a61

--- src/untcfv_msx1.asm
+++ src/untcfv_msx1.asm
@@ -3,187 +3,217 @@
 ;     de = destination
 ; changes: af,af',bc,de,hl,ix
 
+
 UnTCFV::
-	ld	ix,-1           ; last_m_off
+	ld	ix,-1		; last_m_off
+	ld	a,[VDP.DW]
+	inc	a
+	ld	c,a
 
-        ld      a,[hl]          ; read first byte
-        inc     hl
-        rlca
 
-	ex	af,af'
-        ld      a,e		; set destination VRAM address
+	ld	a,d
+	or	01000000b
 	di
-	out     [99h],a
-        ld      a,d
-        or	01000000b
+	out	[c],e		; set destination VRAM address
 	ei
-        out     [99h],a
-	ex	af,af'
+	out	[c],a
+	dec	c
 
-	ld	c,98h
-.litlp: outi
-        inc     de
 
-.loop:  call    GetBit
-        jp      c,.litlp
+	ld	a,[hl]		; read first byte
+	inc	hl
+	rlca
+
+
+.litlp:	outi
+	inc	de
+
+
+.loop:	call	GetBit
+	jp	c,.litlp
 .endlit:
 
-        push    de              ; save dst
-        ld      de,1
-.moff:  call    GetBit
-        rl      e
-        rl      d
-        call    GetBit
-        jr      c,.gotmoff
-        dec     de
-        call    GetBit
-        rl      e
-        rl      d
-        jp      nc,.moff
-        pop     de              ; end of compression
-        ret
+
+	push	de		; save dst
+	ld	de,1
+.moff:	call	GetBit
+	rl	e
+	rl	d
+	call	GetBit
+	jr	c,.gotmoff
+	dec	de
+	call	GetBit
+	rl	e
+	rl	d
+	jp	nc,.moff
+	pop	de		; end of compression
+	ret
+
 
 .gotmoff:
-        ex      af,af'
-        ld      bc,0            ; m_len
-        dec     de
-        dec     de
-        ld      a,e
-        or      d
-        jr      z,.prevdist
-        dec     de
-        ld      a,e
-        cpl
-        srl	d
-        ccf
-        ld      d,a
-        ld      e,[hl]
-        inc     hl
-        rr      d
-        rr      e
-        ld      ixl,e
-        ld      ixh,d
-        jr      c,.mlena
-        ex      af,af'
-        inc     bc
-        call    GetBit
-        jr      c,.mlenx
+	ex	af,af'
+	ld	bc,0		; m_len
+	dec	de
+	dec	de
+	ld	a,e
+	or	d
+	jr	z,.prevdist
+	dec	de
+	ld	a,e
+	cpl
+	srl	d
+	ccf
+	ld	d,a
+	ld	e,[hl]
+	inc	hl
+	rr	d
+	rr	e
+	ld	ixl,e
+	ld	ixh,d
+	jr	c,.mlena
+	ex	af,af'
+	inc	bc
+	call	GetBit
+	jr	c,.mlenx
 
-.mlen:  call    GetBit
-        rl      c
-        rl      b
-        call    GetBit
-        jp      nc,.mlen
-        inc     bc
-        inc     bc
+
+.mlen:	call	GetBit
+	rl	c
+	rl	b
+	call	GetBit
+	jp	nc,.mlen
+	inc	bc
+	inc	bc
 .gotmlen:
-        ex	af,af'
-        ld      a,d
-        cp      -5
-        jp      nc,.nc
-        inc     bc
-.nc:	inc     bc
-        inc     bc
+	ex	af,af'
+	ld	a,d
+	cp	-5
+	jp	nc,.nc
+	inc	bc
+.nc:	inc	bc
+	inc	bc
 
-	ex	[sp],hl		; save src, and get dst in hl, de = offset
-        ex      de,hl           ; de = dst, hl = offset
-        add     hl,de           ; new src = dst+offset
 
-	push	hl	
+	ex	[sp],hl		; save src, and	get dst	in hl, de = offset
+	ex	de,hl		; de = dst, hl = offset
+	add	hl,de		; new src = dst+offset
+
+
+	push	hl
 	inc	hl
 	sbc	hl,de
 	pop	hl
 	jp	z,.unbuffer
 
-	ex	af,af'
-	push	af
+
 .matchlp:
-        ld      a,l
+	push	bc
+	ld	bc,[VDP.DR]
+	ld	a,b
+	inc	a
+	ld	b,c
+	ld	c,a
+
+
 	di
-	out     [99h],a
-        ld      a,h
+	out	[c],l
 	ei
-        out     [99h],a
-	inc	hl
-        in      a,[98h]                 ; read byte
+	out	[c],h
+	ld	c,b
+	in	b,[c]		      ;	read byte
+	ld	c,a
 
-	ex	af,af'
-        ld      a,e
+
+	ld	a,d
+	or	01000000b
 	di
-	out     [99h],a
-        ld      a,d
-        or	01000000b
+	out	[c],e
 	ei
-        out     [99h],a
+	out	[c],a
+	inc	de
+	dec	c
+	out	[c],b		      ;	write byte
+
+
+	pop	bc
+	cpi
+	jp	pe,.matchlp
+
+
+	ld	a,[VDP.DW]
+	ld	c,a
 	ex	af,af'
-        inc	de
-        out     [98h],a                 ; write byte
+	pop	hl			; get src back
+	jp	.loop
 
-        dec     bc
-        ld      a,c
-        or      b
-        jp      nz,.matchlp
-        pop	af
-
-        pop	hl			; get src back
-	ld	c,98h
-        jp      .loop
 
 .prevdist:
-        ex      af,af'
-        ld      e,ixl
-        ld      d,ixh
-        call    GetBit
-        jr      c,.mlenx
-        inc     bc
-        call    GetBit
-        jr      nc,.mlen
-        ex	af,af'
+	ex	af,af'
+	ld	e,ixl
+	ld	d,ixh
+	call	GetBit
+	jr	c,.mlenx
+	inc	bc
+	call	GetBit
+	jr	nc,.mlen
+	ex	af,af'
 
-.mlena:	ex      af,af'
-.mlenx: call    GetBit
-        rl      c
-        jp      .gotmlen
+
+.mlena:	ex	af,af'
+.mlenx:	call	GetBit
+	rl	c
+	jp	.gotmlen
+
 
 .unbuffer:
-        ld      a,l
-	di
-	out     [99h],a
-        ld      a,h
-	ei
-        out     [99h],a
+	push	bc
+	ld	bc,[VDP.DR]
+	ld	a,b
+	inc	a
+	ld	b,c
+	ld	c,a
 
-        in      a,[98h]                 ; read byte
-	ld	l,a
 
-        ld      a,e
 	di
-	out     [99h],a
-        ld      a,d
-        or	01000000b
+	out	[c],l
 	ei
-        out     [99h],a
+	out	[c],h
+	ld	c,b
+	in	b,[c]		      ;	read byte
+	ld	c,a
+
+
+	ld	a,d
+	or	01000000b
+	di
+	out	[c],e
+	ei
+	out	[c],a
+	pop	hl
+
 
 	ex	de,hl
-	add	hl,bc
+	add	hl,de
 	ex	de,hl
 
+
+	dec	c
 .bufmatch:
+	out	[c],b			; write	byte
+	dec	hl
 	ld	a,l
-        out     [98h],a                 ; write byte
-        dec     bc
-        ld      a,c
-        or      b
-        jp      nz,.bufmatch
-        ex	af,af'
+	or	h
+	jp	nz,.bufmatch
 
-        pop	hl			; get src back
-	ld	c,98h
-        jp      .loop
 
-GetBit: add     a,a
-        ret     nz
-        ld      a,[hl]          ; read new byte
-        inc     hl
-        adc     a,a             ; cf = 1, last bit shifted is always 1
-        ret
+	ex	af,af'
+	pop	hl			; get src back
+	jp	.loop
+
+
+GetBit:	add	a,a
+	ret	nz
+	ld	a,[hl]		; read new byte
+	inc	hl
+	adc	a,a		; cf = 1, last bit shifted is always 1
+	ret
